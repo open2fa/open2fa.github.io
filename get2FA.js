e53c5c66ab20f3f1eb46d5a82ea151e402219cab
fibo <fibo@users.noreply.github.com>
2023-08-05 13:49:57 +0200

    support digits longer than 6

--- get2FA.js
+++ get2FA.js
@@ -2,21 +2,36 @@
 // Open2FA project came to life thanks to this function that Mathias Lasser
 // kindly shared with me during a nighlty coding session.
 // It was my first meet up at Ready2Order in Vienna, during May 2023.
-//
-// Notice that indentation is preserved as is, this is Matthias' coding style.
-/* prettier-ignore */
-export function get2FA(sharedSecret){
-  const alphabet=Object.fromEntries(Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ234567',(c,i)=>[c,i.toString(2).padStart(5,'0')]))
-  const keyData=new Uint8Array(Array.from(sharedSecret,a=>alphabet[a]).join('').match(/.{8}/g).map(a=>+`0b${a}`));
-  return crypto.subtle.importKey('raw',keyData,{
-    name:'HMAC',
-    hash:'SHA-1'
-  },false,['sign']).then(key=>{
-    const dv=new DataView(new ArrayBuffer(8));
-    dv.setUint32(4,Date.now()/30000);
-    return crypto.subtle.sign(key.algorithm,key,dv).then(signature=>{
-      const dv=new DataView(signature);
-      return ('00000'+(dv.getUint32(dv.getUint8(19)&15)&(-1>>>1))).slice(-6);
-    });
-  });
-}
+const get2FA = (
+  s,
+  d = 6,
+  t = Date.now() / 30000,
+  g = crypto.subtle,
+  u = Uint8Array,
+) =>
+  g
+    .importKey(
+      "raw",
+      u.from(
+        Array.from(s, (c) =>
+          (+c ? +c + 56 : c.charCodeAt(0) - 1).toString(2).slice(-5),
+        )
+          .join("")
+          .match(/.{8}/g),
+        (b) => +("0b" + b),
+      ),
+      { name: "HMAC", hash: "SHA-1" },
+      0,
+      ["sign"],
+    )
+    .then((k) =>
+      g
+        .sign(k.algorithm, k, u.from([, , , , t >> 24, t >> 16, t >> 8, t]))
+        .then((s) =>
+          ((e) =>
+            (u + 1e9 + ((e.getUint32(e.getUint8(19) & 15) << 1) >>> 1)).slice(
+              -d,
+            ))(new DataView(s)),
+        ),
+    );
+
